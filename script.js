9ef326ab81a0d7599c5f050e8d31b5d8984eee16
Julia Khayrulina <julia748@mail.ru>
2021-07-30 16:53:48 +0300

    assign a value to variables and output to the console

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 100000;
+let income = 'freelancing';
+let addExpenses = 'Продукты, Интернет, Моб.связь, Коммуналка, Бензин';
+let deposit = false;
+const mission = 500000;
+let period = 12;
+let budgetDay = Math.floor(money / 30);
 
-alert('hello');
-console.log('Hi');
+//alert('hello');
+console.log('Hello');
+
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLowerCase().split(' '));
+console.log(budgetDay);
